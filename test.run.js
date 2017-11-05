1f00e853b8d5e2640ee8bab8d6515d1d579f8fc7
Vasiliy Vanchuk <v.vanchuk@tut.by>
2017-11-05 20:52:38 +0300

    Add getTranslates

--- test.run.js
+++ test.run.js
@@ -1,9 +1,16 @@
 const { email, password } = require("./config.json");
+
+const util = require("util");
+
 const lingualeoApi = require("./lib/lingualeo");
+
+const WORD = "api";
 
 console.log(`Test runner will use credentials: ${email} : ${password}`);
 
 lingualeoApi
   .login(email, password)
-  .then(data => console.log("Login:", JSON.stringify(data)))
+  // .then(data => console.log("Login:", util.inspect(data)))
+  .then(_ => lingualeoApi.getTranslates(WORD))
+  .then(data => console.log(`Translates for ${WORD}`, util.inspect(data)))
   .catch(err => console.error("Error", err));
