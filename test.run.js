9b4c2c683a10ee30184bad12cad16e33ace421f4
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-12-21 19:09:10 +0300

    Add createWordSet method

--- test.run.js
+++ test.run.js
@@ -6,6 +6,7 @@ const lingualeoApi = require("./lib/lingualeo");
 
 const WORD = "api";
 const RU_WORD = "программист";
+const WORD_SET_TITLE = "test word set";
 
 console.log(`Test runner will use credentials: ${email} : ${password}`);
 
@@ -14,15 +15,19 @@ lingualeoApi
   // .then(data => console.log("Login:", util.inspect(data)))
   .then(_ => lingualeoApi.getTranslates(WORD))
   .then(data => {
-    console.log(`Translates for ${WORD}`, util.inspect(data));
+    console.log(`Translates for ${WORD}`, util.inspect(data.value));
     return data;
   })
   .then(list => lingualeoApi.addWord(WORD, list[0].value))
-  .then(data => console.log("addWord", util.inspect(data)))
+  .then(data => console.log("addWord", util.inspect(data.text)))
   .then(list => lingualeoApi.translate(RU_WORD, "ru", "en"))
   .then(translation =>
     console.log(
       `translation for "${RU_WORD}" from 'ru' to 'en' is "${translation}"`
     )
+  )
+  .then(() => lingualeoApi.addWordSet(WORD_SET_TITLE))
+  .then(wordset =>
+    console.log(`Wordset #${wordset.id} with title "${wordset.name}" was added`)
   )
   .catch(err => console.error("Error", err));
